ece9fe236f984906cbe20cb2da48a1344c3ec1ee
Chris Nager <cnager@gmail.com>
2018-08-02 10:47:49 -0400

    Simplify scripts, add prettier, rebuild project

--- src.js
+++ src.js
@@ -1,22 +1,21 @@
-import GiiKER from 'giiker';
+import GiiKER from './giiker'
 
-const button = document.querySelector('button');
-const textarea = document.querySelector('textarea');
+const button = document.querySelector('button')
+const textarea = document.querySelector('textarea')
 
-button.style.width = '100%';
-button.style.height = '44px';
-button.style.marginBottom = '8px';
-
-textarea.style.boxSizing = 'border-box';
-textarea.style.width = '100%';
-textarea.style.height = 'calc(100vh - 48px - 24px)';
-textarea.style.fontSize = '32px';
+button.style.width = '100%'
+button.style.height = '44px'
+button.style.marginBottom = '8px'
+textarea.style.boxSizing = 'border-box'
+textarea.style.width = '100%'
+textarea.style.height = 'calc(100vh - 48px - 24px)'
+textarea.style.fontSize = '32px'
 
 button.addEventListener('click', async () => {
-  button.disabled = true;
-  const giiker = await GiiKER.connect();
-  button.textContent = 'Connected';
+  button.disabled = true
+  const giiker = await GiiKER.connect()
+  button.textContent = 'Connected'
   giiker.on('move', move => {
-    textarea.value += ` ${move.notation}`;
-  });
-});
+    textarea.value += ` ${move.notation}`
+  })
+})
