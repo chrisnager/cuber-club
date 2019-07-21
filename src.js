4ccc1343514e11cf9bd1e9ec6541a5d8fa190f21
Chris Nager <cnager@gmail.com>
2019-07-21 12:06:31 -0400

    Add sounds

--- src.js
+++ src.js
@@ -3,13 +3,43 @@ import GiiKER from './giiker'
 const button = document.querySelector('button')
 const textarea = document.querySelector('textarea')
 
-// button.style.width = '100%'
-// button.style.height = '44px'
-// button.style.marginBottom = '8px'
-// textarea.style.boxSizing = 'border-box'
-// textarea.style.width = '100%'
-// textarea.style.height = 'calc(20vh - 48px - 24px)'
-// textarea.style.fontSize = '32px'
+const cubeSounds = {
+  kicka: { sound: new Howl({ src: ['samples/Kick.wav'] }) },
+  kickb: { sound: new Howl({ src: ['samples/Kick Accent.wav'] }) },
+  snarea: { sound: new Howl({ src: ['samples/Snare.wav'] }) },
+  snareb: { sound: new Howl({ src: ['samples/Snare Accent.wav'] }) },
+  rim: { sound: new Howl({ src: ['samples/Rim Shot.wav'] }) },
+  hihata: { sound: new Howl({ src: ['samples/HiHat.wav'] }) },
+  hihatb: { sound: new Howl({ src: ['samples/HiHat Accent.wav'] }) },
+  hihatc: { sound: new Howl({ src: ['samples/HiHat Metal.wav'] }) },
+  cymbal: { sound: new Howl({ src: ['samples/Cymbal.wav'] }) },
+  bongohigh: { sound: new Howl({ src: ['samples/Bongo High.wav'] }) },
+  bongolow: { sound: new Howl({ src: ['samples/Bongo Low.wav'] }) },
+  congalow: { sound: new Howl({ src: ['samples/Conga Low.wav'] }) },
+  cowbell: { sound: new Howl({ src: ['samples/Cowbell.wav'] }) },
+  tamb1: { sound: new Howl({ src: ['samples/Tamb 1.wav'] }) },
+  tamb2: { sound: new Howl({ src: ['samples/Tamb 2.wav'] }) }
+}
+
+const moveMap = {
+  R: 'kicka',
+  "R'": 'kickb',
+  L: 'snarea',
+  "L'": 'snareb',
+  U: 'rim',
+  "U'": 'hihata',
+  D: 'hihatb',
+  "D'": 'hihatc',
+  F: 'cymbal',
+  "F'": 'bongohigh',
+  B: 'bongolow',
+  "B'": 'congalow'
+}
+
+const playSound = move => {
+  console.log(`Play sound "${move}"`)
+  cubeSounds[moveMap[move]].sound.play()
+}
 
 button.addEventListener('click', async () => {
   button.disabled = true
@@ -18,6 +48,6 @@ button.addEventListener('click', async () => {
   giiker.on('move', move => {
     // const currentTextareaValue = textarea.value
     textarea.value += ` ${move.notation}`
-    // div.setAttribute('data-config', `alg=${(currentTextareaValue += ` ${move.notation}`)}| colored=U*| flags=showalg|hover=none`)
+    playSound(move.notation)
   })
 })
