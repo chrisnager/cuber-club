6778465cada0742888f429f621a6d622913f1db1
Chris Nager <cnager@gmail.com>
2019-08-04 12:15:36 -0400

    Remove inline styles

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,23 +1,17 @@
 /** @jsx jsx */
-import { jsx, Styled } from 'theme-ui'
+import { jsx } from 'theme-ui'
 
 export default function Header() {
   return (
-    <header
-      sx={{
-        fontWeight: 'bold',
-        fontSize: 4,
-        color: 'primary'
-      }}
-    >
+    <header>
       <div>
-        <Styled.h1>
+        <h1>
           <img src="/static/images/logo.svg" alt="Logo" /> Cube DJ
-        </Styled.h1>
-        <Styled.p>
+        </h1>
+        <p>
           Use your <a href="https://www.thecubicle.com/products/giiker-super-cube-i3s">Bluetooth-enabled smart cube</a> as a MIDI input
           device.
-        </Styled.p>
+        </p>
         <img src="https://media.giphy.com/media/vqYM1Ei0CcepW/giphy.gif" alt="Cube in motion" />
       </div>
     </header>
