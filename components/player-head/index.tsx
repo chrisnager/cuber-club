92d9a018dce2e8cc7b7c7b461c1eacb864ad54e5
Chris Nager <cnager@gmail.com>
2019-08-04 23:24:42 -0400

    Adjust player header

--- components/player-head/index.tsx
+++ components/player-head/index.tsx
@@ -10,13 +10,14 @@ export default function PlayerHead({
   return (
     <>
       <tr>
-        <th colSpan={2}>Moves</th>
+        <th colSpan={2}>
+          <button onClick={handlePlayPause}>{paused ? 'Play' : 'Pause'}</button>
+        </th>
         <th colSpan={tracksLength}>
           <div>
             <button disabled={!paused} onClick={handleLeftClick}>
               ↤
             </button>
-            <button onClick={handlePlayPause}>{paused ? 'Play' : 'Pause'}</button>
             <button disabled={!paused} onClick={handleRightClick}>
               ↦
             </button>
@@ -24,7 +25,7 @@ export default function PlayerHead({
         </th>
       </tr>
       <tr>
-        <th colSpan={2}>-------------</th>
+        <th colSpan={2}>Moves</th>
         <th colSpan={tracksLength}>
           <input disabled={!paused} type="range" max={tracksLength - 1} value={position} onChange={handleInputChange} />
         </th>
