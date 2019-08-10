0364f151cbb0d268e79bb635f529aaedcb92afae
Chris Nager <cnager@gmail.com>
2019-08-10 00:02:17 -0400

    Add button titles

--- components/player-controls/index.tsx
+++ components/player-controls/index.tsx
@@ -1,11 +1,13 @@
 export default function PlayerControls({ handlePlayPause, paused, handleLeftClick, handleRightClick }) {
   return (
     <div>
-      <button onClick={handlePlayPause}>{paused ? '▶ (B)' : '❚❚ (B)'}</button>
-      <button disabled={!paused} onClick={handleLeftClick}>
+      <button title={paused ? 'Play' : 'Pause'} onClick={handlePlayPause}>
+        {paused ? '▶ (B)' : '❚❚ (B)'}
+      </button>
+      <button title="Move left" disabled={!paused} onClick={handleLeftClick}>
         ↤ (D)
       </button>
-      <button disabled={!paused} onClick={handleRightClick}>
+      <button title="Move right" disabled={!paused} onClick={handleRightClick}>
         (D') ↦
       </button>
     </div>
