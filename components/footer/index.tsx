e034bab606e160f2b39f3a525fc1f18f685b569d
Chris Nager <cnager@gmail.com>
2019-08-10 01:57:58 -0400

    Add footer info

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -6,6 +6,10 @@ export default function Footer() {
           <a href="https://caniuse.com/#feat=web-bluetooth">Web Bluetooth support</a> is currently limited to Chrome, Opera, and Edge.
         </p>
         <p>
+          Built with NextJS, React, TypeScript, Howler, ThemeUI, Web Bluetooth API, Web Audio API, Giiker API, accessibilty, and
+          imagination. See the source code on <a href="https://github.com/chrisnager/smart-cube">GitHub</a>.
+        </p>
+        <p>
           &copy; {new Date().getFullYear()} &middot; Crafted by <a href="http://chrisnager.com">Chris Nager</a>
         </p>
       </div>
